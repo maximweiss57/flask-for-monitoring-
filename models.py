8205daf822df39264bbb05d2d4664c95eabc2872
Maxim weiss <137171905+maximweiss57@users.noreply.github.com>
2024-03-04 15:13:51 +0200

    build 0.5

--- models.py
+++ models.py
@@ -1,3 +1,18 @@
 from flask_pymongo import PyMongo
 
 mongo = PyMongo()
+
+class VoteResult:
+    @classmethod
+    def insert_vote(cls, parameter):
+        # Insert or update vote count in MongoDB
+        mongo.db.vote_results.update_one(
+            {"parameter": parameter},
+            {"$inc": {"count": 1}},
+            upsert=True
+        )
+
+    @classmethod
+    def get_all_results(cls):
+        # Retrieve all vote results from MongoDB
+        return mongo.db.vote_results.find()
