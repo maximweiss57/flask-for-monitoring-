0af0be07cebc133a8b0d3a7a3fffd4e4e58d519e
Maxim weiss <137171905+maximweiss57@users.noreply.github.com>
2024-03-11 16:49:58 +0200

    Refactor MongoDB connection and update parameter names in index.html

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, Blueprint, request, jsonify, render_template
-from flask_pymongo import PyMongo
-
-mongo = PyMongo()
+from pymongo import MongoClient
+import os   
 
 class VoteResult:
     @classmethod
-    def insert_vote(cls, parameter):
+    def insert_vote(cls, parameter, mongo):
         # Insert or update vote count in MongoDB
         mongo.db.vote_results.update_one(
             {"parameter": parameter},
@@ -14,7 +13,7 @@ class VoteResult:
         )
 
     @classmethod
-    def get_all_results(cls):
+    def get_all_results(cls, mongo):
         # Retrieve all vote results from MongoDB
         return mongo.db.vote_results.find()
 
@@ -30,7 +29,7 @@ def vote():
     parameter = request.json.get('parameter')
 
     # Update vote count in MongoDB
-    VoteResult.insert_vote(parameter)
+    VoteResult.insert_vote(parameter, mongo)
 
     return jsonify({"message": "Vote submitted successfully"})
 
@@ -38,31 +37,38 @@ def vote():
 @survey_routes.route('/results')
 def survey_results():
     # Assuming you have a collection named 'vote_results' in your MongoDB
-    survey_data = VoteResult.get_all_results()
+    survey_data = VoteResult.get_all_results(mongo)
     parameters = []
     for data in survey_data:
         parameters.append({'name': data['parameter'], 'value': data['count']})
     return render_template('results.html', parameters=parameters)
+
+mongo_username = os.environ.get('MONGO_INITDB_ROOT_USERNAME')
+mongo_password = os.environ.get('MONGO_INITDB_ROOT_PASSWORD')
+mongo_host = os.environ.get('MONGO_HOST')
+mongo_port = os.environ.get('MONGO_PORT')
+
 def create_app():
     app = Flask(__name__)
     
+
     # Configuration for MongoDB
-    # mongo_uri = f'mongodb://localhost:27017/survey_db'
-    mongo_uri = f'mongodb://admin:admin@172.18.255.200:27017/survey_db'
+    mongo_uri = f'mongodb://{mongo_username}:{mongo_password}@{mongo_host}:{mongo_port}/'
+    client = MongoClient(mongo_uri)
+    global mongo
+    mongo = client.survey_db
+    
     app.config["MONGO_URI"] = mongo_uri
 
     try:
-        mongo.init_app(app)
-
         # Initialize MongoDB with the required collections
-        with app.app_context():
-            if 'vote_results' not in mongo.db.list_collection_names():
-                # Create 'vote_results' collection if it doesn't exist
-                mongo.db.create_collection('vote_results')
-                # Initialize vote counts to 0 for each parameter
-                initial_parameters = ['parameter1', 'parameter2', 'parameter3']  # Add your parameters here
-                for param in initial_parameters:
-                    mongo.db.vote_results.insert_one({"parameter": param, "count": 0})
+        if 'vote_results' not in mongo.list_collection_names():
+            # Create 'vote_results' collection if it doesn't exist
+            mongo.create_collection('vote_results')
+            # Initialize vote counts to 0 for each parameter
+            initial_parameters = ['parameter1', 'parameter2', 'parameter3']  # Add your parameters here
+            for param in initial_parameters:
+                mongo.vote_results.insert_one({"parameter": param, "count": 0})
     except Exception as e:
         print(f"Error initializing MongoDB: {e}")
 
@@ -71,8 +77,7 @@ def create_app():
 
     return app
 
-
-app=create_app()
+app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0', port=5000)
