4d8438c752c49d531a03fd512c6a64b77fd65dfd
Maxim weiss <137171905+maximweiss57@users.noreply.github.com>
2024-03-11 15:03:42 +0200

    Refactor MongoDB URI in app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ def create_app():
     app = Flask(__name__)
     
     # Configuration for MongoDB
-    app.config['MONGO_URI'] = 'mongodb://admin:admin@172.18.255.200:27017/survey_db'
+    mongo_uri = f'mongodb://admin:admin@172.18.255.200:27017/'
+    app.config["MONGO_URI"] = mongo_uri
     mongo.init_app(app)
     
     # Register blueprints
