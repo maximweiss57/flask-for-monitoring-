0b26c220f74956d7e931ce948d17dd56a306da44
Maxim weiss <137171905+maximweiss57@users.noreply.github.com>
2024-03-11 14:48:09 +0200

    Update MongoDB URI in app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def create_app():
     app = Flask(__name__)
     
     # Configuration for MongoDB
-    app.config['MONGO_URI'] = 'mongodb://27017/survey_db'
+    app.config['MONGO_URI'] = 'mongodb://admin:admin@172.18.255.200:27017/survey_db'
     mongo.init_app(app)
     
     # Register blueprints
